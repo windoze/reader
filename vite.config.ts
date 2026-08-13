356737e9d586ef32959a6b133a26acaa0f7fe203
徐辰 <windoze@0d0a.com>
2026-08-13 17:55:34 +0800

    Tauri initialized

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  clearScreen: false,
+  server: {
+    host: process.env.TAURI_DEV_HOST ?? "127.0.0.1",
+    port: 5173,
+    strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/**"]
+    }
+  }
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    setupFiles: ["./src/ui/test/setup.ts"],
+    setupFiles: ["./src/test/setup.ts"],
     globals: true
   }
 });
